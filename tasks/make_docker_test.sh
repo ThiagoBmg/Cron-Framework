7ec9d969e23e356cc16fd5f7d37e48098e6e63fc
Thiago <th.bmg5274@gmail.com>
2021-09-22 01:26:48 -0300

    otimizando script de teste do docker

--- tasks/make_docker_test.sh
+++ tasks/make_docker_test.sh
@@ -1,4 +1,9 @@
 echo 'removendo imagens anteriores'
-docker rm -f $(docker ps -a -q);
-docker rmi -f $(docker images -q);
-./docker_build.sh
+docker rm -f cron_job_teste;
+docker rmi -f cron_job_teste_image;
+./docker_build.sh;
+
+echo ' criando nova imagem';
+docker build -t cron_job_teste_image .. &&
+docker run --name "cron_job_teste" cron_job_teste_image;
+echo 'processo finalizado'
