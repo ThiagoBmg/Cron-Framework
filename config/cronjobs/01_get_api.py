876a5fcf9fe69b01d70effaf52461ae42299d5ad
Thiago <th.bmg5274@gmail.com>
2021-09-22 10:30:13 -0300

    adicionando nova api e ajustando o tempo de execução do job

--- config/cronjobs/01_get_api.py
+++ config/cronjobs/01_get_api.py
@@ -2,23 +2,28 @@ import requests
 import os
 import logging
 
+# log config
 current_dir  = os.path.dirname(os.path.abspath(__file__))
 logging.basicConfig(filename=f"{current_dir}/log/cronjobs.log", 
 level=logging.DEBUG, 
 format=f'[%(asctime)s] %(levelname)s - [%(filename)s]: %(message)s')
 
-def getSomething():
+#https://aviationstack.com/dashboard
+#https://aviationstack.com/documentation
+API_KEY = '44029ca0634b1f7d532133ad687d4554'
+BASE_URL = 'http://api.aviationstack.com/v1'
+
+def step01():
     logging.info(f'iniciando job API')
     request = requests.get(
-        url= 'https://api.github.com/user/',
-        headers={"accept": "application/vnd.github.v3+json"},
-        params={"name":"ThiagoBmg"}
+        url= f'{BASE_URL}/flights',
+        params={"access_key": API_KEY}
     )
     logging.debug(f'api response {request.json()}')
     logging.info(f"concluindo job API")
     pass
 
 
-getSomething()
+step01()
 
    
