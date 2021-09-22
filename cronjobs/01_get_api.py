568c9c290583f4cd392f694d1080f2a47d6e2b62
Thiago <th.bmg5274@gmail.com>
2021-09-22 09:52:52 -0300

    ajustando log

--- cronjobs/01_get_api.py
+++ cronjobs/01_get_api.py
@@ -3,17 +3,19 @@ import os
 import logging
 
 current_dir  = os.path.dirname(os.path.abspath(__file__))
-logging.basicConfig(filename=f"{current_dir}/log/cronjobs.log", level=logging.DEBUG, format=f'[%(asctime)s] %(levelname)s - [%(filename)s]: %(message)s')
+logging.basicConfig(filename=f"{current_dir}/log/cronjobs.log", 
+level=logging.DEBUG, 
+format=f'[%(asctime)s] %(levelname)s - [%(filename)s]: %(message)s')
 
 def getSomething():
-    logging.warning(f'iniciando job API')
+    logging.info(f'iniciando job API')
     request = requests.get(
         url= 'https://api.github.com/user/',
         headers={"accept": "application/vnd.github.v3+json"},
         params={"name":"ThiagoBmg"}
     )
-    logging.error(f'Usuário localizado com sucesso -> ${request.json()}')
-    logging.debug(f"concluindo job API")
+    logging.debug(f'api response {request.json()}')
+    logging.info(f"concluindo job API")
     pass
 
 
